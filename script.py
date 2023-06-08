2cd3a5d077ec5654f1436d611f4ea5c28be51607
MohamedKamis <mhmdaljwokr935@gmail.com>
2023-06-08 18:41:28 +0300

    first commit2

--- script.py
+++ script.py
@@ -3,18 +3,18 @@ data1= sys.argv
 del data1[0]
 data1= ' '.join(data1)
 
-from pytube import YouTube
+# from pytube import YouTube
 
-url = (data1)
+# url = (data1)
 
 
 
-yt = YouTube(url)
-List=[]
-for stream in yt.streams:
-    if(stream.resolution!=None ):
-        List.append(stream.resolution)
-my_set = set(List)
+# yt = YouTube(url)
+# List=[]
+# for stream in yt.streams:
+#     if(stream.resolution!=None ):
+#         List.append(stream.resolution)
+# my_set = set(List)
 
-data1= ' '.join(my_set)
+# data1= ' '.join(my_set)
 print(data1)
